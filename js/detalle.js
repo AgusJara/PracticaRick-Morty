033c7f30ab7340e92a8ede98463343f9823afe14
AgusJara <ajaralambides@udesa.edu.ar>
2024-05-31 12:57:15 -0300

    Rick&Morty Practica Parte 1

---
+++ js/detalle.js
@@ -0,0 +1,26 @@
+let query = location.search;
+let queryStringObj = new URLSearchParams(query);
+let id = queryStringObj.get("id");
+
+fetch (`https://rickandmortyapi.com/api/character/${id}`)
+    .then (function(response){
+        return response .json();
+    })
+    .then (function(data){
+        console.log(data);
+        let nombrePerso = document.querySelector ('.nombre');
+        let statusPerso = document.querySelector ('.status');
+        let especiePerso = document.querySelector ('.especie');
+        let imagenPerso = document.querySelector ('.imagen');
+
+        nombrePerso.innerText = data.name;
+        statusPerso.innerText += data.status;
+        especiePerso.innerText += data.species;
+        imagenPerso.src = data.image;
+        imagenPerso.alt = `Imagen de ${data.name}`;
+    })
+    .catch(function(error){
+        console.log (error);
+    })
+        
+    
